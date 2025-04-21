274d800c2881f1f76f08faf0e45eded0d66e994b
erfanian <erfanaliyavari@gmail.com>
2025-04-21 18:49:03 +0330

    added all the views and templates

--- core_apps/app/urls.py
+++ core_apps/app/urls.py
@@ -1,8 +1,23 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import AuthorListView, AuthorCreateView
+from .views import (
+    AuthorListView,
+    AuthorCreateView,
+    AuthorUpdateView,
+    AuthorDeleteView,
+    AuthorDetailView,
+    BookCreateView,
+    BookUpdateView,
+    BookDeleteView,
+)
 
 urlpatterns = [
     path("", AuthorListView.as_view(), name="home"),
     path("add_author", AuthorCreateView.as_view(), name="add_author"),
+    path("edit_author/<int:pk>", AuthorUpdateView.as_view(), name="edit_author"),
+    path("delete_author/<int:pk>", AuthorDeleteView.as_view(), name="delete_author"),
+    path("author_detail/<int:pk>", AuthorDetailView.as_view(), name="author_detail"),
+    path("add_book/<int:pk>", BookCreateView.as_view(), name="add_book"),
+    path("edit_book/<int:pk>", BookUpdateView.as_view(), name="edit_book"),
+    path("delete_book/<int:pk>", BookDeleteView.as_view(), name="delete_book"),
 ]
--- core_apps/app/views.py
+++ core_apps/app/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-from django.views.generic import ListView, CreateView, UpdateView, DeleteView
-from django.urls import reverse_lazy
+from django.views.generic import (
+    ListView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+    DetailView,
+)
+from django.urls import reverse_lazy, reverse
 from .models import Author, Book
 from django.db.models import Q
+from django.shortcuts import get_object_or_404, redirect
 
 # Create your views here.
 
@@ -12,19 +19,81 @@ class AuthorListView(ListView):
     template_name = "app/home.html"
     context_object_name = "authors"
 
-    # def get_queryset(self):
-    #     query = self.request.GET.get("q")
-    #     if query:
-    #         return Author.objects.filter(
-    #             Q(first_name__icontains=query)
-    #             | Q(last_name__icontains=query)
-    #             | Q(national_id__icontains=query)
-    #         )
-    #     return Author.objects.all()
+    def get_queryset(self):
+        query = self.request.GET.get("q")
+        print(query)
+        if query:
+            return Author.objects.filter(
+                Q(first_name__icontains=query)
+                | Q(last_name__icontains=query)
+                | Q(national_id__icontains=query)
+            )
+        return Author.objects.all()
 
 
 class AuthorCreateView(CreateView):
     model = Author
     fields = ["first_name", "last_name", "age", "national_id"]
+
     template_name = "app/add_author_form.html"
     success_url = reverse_lazy("home")
+
+
+class AuthorUpdateView(UpdateView):
+    model = Author
+    fields = ["first_name", "last_name", "age", "national_id"]
+    context_object_name = "author"
+    template_name = "app/edit_author_form.html"
+    success_url = reverse_lazy("home")
+
+
+class AuthorDeleteView(DeleteView):
+    model = Author
+    template_name = "app/delete_author.html"
+    success_url = reverse_lazy("home")
+
+
+class AuthorDetailView(DetailView):
+    model = Author
+    template_name = "app/author_detail.html"
+    context_object_name = "author"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        book_query = self.request.GET.get("q")
+        books = self.object.books.all()
+        if book_query:
+            books = books.filter(title__icontains=book_query)
+        context["books"] = books
+        return context
+
+
+class BookCreateView(CreateView):
+    model = Book
+    fields = ["title", "publication_year"]
+    template_name = "app/add_book_form.html"
+
+    def form_valid(self, form):
+        form.instance.author = get_object_or_404(Author, id=self.kwargs["pk"])
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse("author_detail", kwargs={"pk": self.kwargs["pk"]})
+
+
+class BookUpdateView(UpdateView):
+    model = Book
+    fields = ["title", "publication_year", "author"]
+    template_name = "app/edit_book_form.html"
+
+    def get_success_url(self):
+        return reverse("author_detail", kwargs={"pk": self.object.author.id})
+
+
+class BookDeleteView(DeleteView):
+    model = Book
+    template_name = "app/delete_book.html"
+    context_object_name = "book"
+
+    def get_success_url(self):
+        return reverse("author_detail", kwargs={"pk": self.object.author.id})
