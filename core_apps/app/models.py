d223462aab1d4059bf80b19846c788abf93a6392
erfanian <erfanaliyavari@gmail.com>
2025-04-21 19:04:42 +0330

    added max age and year validators

--- core_apps/app/models.py
+++ core_apps/app/models.py
@@ -1,12 +1,22 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.utils import timezone
+from django.core.validators import MaxValueValidator
+
 
 # Create your models here.
+def validate_publication_year(value):
+    current_year = timezone.now().year
+    if value > current_year:
+        raise ValidationError(
+            f"Publication year can't be in the future (max {current_year})."
+        )
 
 
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    age = models.PositiveIntegerField()
+    age = models.PositiveIntegerField(validators=[MaxValueValidator(199)])
     national_id = models.CharField(max_length=10, unique=True)
 
     def __str__(self):
@@ -15,7 +25,9 @@ class Author(models.Model):
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
-    publication_year = models.PositiveIntegerField()
+    publication_year = models.PositiveIntegerField(
+        validators=[validate_publication_year]
+    )
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="books")
 
     def __str__(self):
